d178fc18fc9a31eabc9c7911a94459bf878cfb3e
Latnipgit <gitlatnip91@gmail.com>
2024-03-20 20:52:42 +0530

    admin delete $

--- app/routes/admin/admin.routes.js
+++ app/routes/admin/admin.routes.js
@@ -9,26 +9,26 @@ module.exports = app => {
     router.post("/add", auth, admin.addAdmin);
     router.post("/logout", auth, admin.logout);
     router.get("/getLoginInfo", auth, admin.getLoginInfo);
-    router.get("/getAllAdmins",auth,admin.getAllAdmins);
-    router.post("/getalltransactions",auth,admin.getAllTransactions);
-    router.post("/getalltransactionsMerged",auth,admin.getalltransactionsMerged);
-    router.get("/getAllUsers",auth, admin.getAllUsers);
-    router.post("/escalateRequest",auth, admin.escalateRequest);
-    router.post("/removeQuestionById",auth, admin.removeQuestion);
-    router.get("/companiesFilter",auth, admin.companiesFilter);
-    router.get("/getAllCompanies",auth, admin.getAllCompanies);
-    router.post("/getCompanyCountStateWise",auth, admin.getCompanyCountStateWise);
-    router.post("/getCompanyCountCityWiseForState",auth, admin.getCompanyCountCityWiseForState);
-    router.post("/getDisputedTransactionsStateCityWise",auth, admin.getDisputedTransactionsStateCityWise);
+    router.get("/getAllAdmins", auth, admin.getAllAdmins);
+    router.post("/getalltransactions", auth, admin.getAllTransactions);
+    router.post("/getalltransactionsMerged", auth, admin.getalltransactionsMerged);
+    router.get("/getAllUsers", auth, admin.getAllUsers);
+    router.post("/escalateRequest", auth, admin.escalateRequest);
+    router.post("/removeQuestionById", auth, admin.removeQuestion);
+    router.get("/companiesFilter", auth, admin.companiesFilter);
+    router.get("/getAllCompanies", auth, admin.getAllCompanies);
+    router.post("/getCompanyCountStateWise", auth, admin.getCompanyCountStateWise);
+    router.post("/getCompanyCountCityWiseForState", auth, admin.getCompanyCountCityWiseForState);
+    router.post("/getDisputedTransactionsStateCityWise", auth, admin.getDisputedTransactionsStateCityWise);
     // router.get("/getDefaulterCountForSelectedCompanies", auth, admin.getDefaulterCountForSelectedCompanies);
 
     // subscription pkg routes
-    router.post("/addSubscriptionPkg",auth, admin.addSubscriptionPkg);
-    router.post("/getServicesListForSubPkg",auth, admin.getServicesListForSubPkg);
-    router.get("/getAllSubscriptionPkg",auth, admin.getAllSubscriptionPkg);
-    router.post("/getSubscriptionPkgById",auth, admin.getSubscriptionPkgById);
-    router.post("/updateSubscriptionPkgById",auth, admin.updateSubscriptionPkgById);
-    router.post("/deleteSubscriptionPkg",auth, admin.deleteSubscriptionPkg);
+    router.post("/addSubscriptionPkg", auth, admin.addSubscriptionPkg);
+    router.post("/getServicesListForSubPkg", auth, admin.getServicesListForSubPkg);
+    router.get("/getAllSubscriptionPkg", auth, admin.getAllSubscriptionPkg);
+    router.post("/getSubscriptionPkgById", auth, admin.getSubscriptionPkgById);
+    router.post("/updateSubscriptionPkgById", auth, admin.updateSubscriptionPkgById);
+    router.post("/deleteSubscriptionPkg", auth, admin.deleteSubscriptionPkg);
 
     // subscription pkg quota mapping routes
     router.post("/addSubPkgAPIQtMapping", auth, admin.addSubPkgAPIQtMapping);
@@ -40,9 +40,11 @@ module.exports = app => {
     router.post("/login", admin.authenticateAdmin);
     router.post("/refreshToken", admin.refreshToken);
     router.post("/changePasswordUsingToken", admin.changePasswordUsingToken);
-    router.post("/changePasswordUsingOldPass",auth, admin.changePasswordUsingOldPass);
+    router.post("/changePasswordUsingOldPass", auth, admin.changePasswordUsingOldPass);
     router.post("/forgetPassword", admin.forgetPassword);
     router.post("/password-reset/:userId/:token", admin.forgetPasswordLink);
+
+    router.post("/delete", auth, admin.deleteUser);
 
     app.use("/api/admin", router);
 };
