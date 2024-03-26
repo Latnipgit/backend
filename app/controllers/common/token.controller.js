19928cf891ddf81517fbcdfe9de4253452a92648
Latnipgit <gitlatnip91@gmail.com>
2024-03-26 11:13:53 +0530

    extras removal

--- app/controllers/common/token.controller.js
+++ app/controllers/common/token.controller.js
@@ -2,36 +2,6 @@ const db = require("../../models/common/");
 const commonService = require("../../service/common");
 const TokenService = commonService.tokenService;
 
-// exports.getMailTemplate = async(mailType, replacements) => {
-//     var template;
-//     try {
-//         template = await tokens.findOne({ mailType: mailType });
-
-//         for (i = 0; i < replacements.length; i++) {
-//             console.log(replacements[i])
-//             template.description= template.description.replaceAll("{{"+replacements[i].target+"}}", replacements[i].value)
-//         }
-//     } catch (err) {
-//         console.log(err)
-//     }
-
-//     return template;
-// };
-
-
-// exports.getAllMailTemplate = async(req, res) => {
-//     let templates;
-//     try {
-//         templates = await MailTemplates.find();
-//         res.status(200).json({message: '.', success: true, response: templates});
-//     } catch (err) {
-//         console.log(err)
-//     }
-
-// };
-
-
-
 exports.saveTokenToDb = async(req, res) => {
     try {
 
